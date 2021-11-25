4d4563cdadc58281a3b3b4f3513e8f86bb146ca9
cheonjoosung <cjswntjd@gmail.com>
2021-11-25 23:21:16 +0900

    라이브러리 레이아웃 & 생성자 구현

--- simple_seekbar/src/main/java/com/js/simple_seekbar/SimpleSeekBar.kt
+++ simple_seekbar/src/main/java/com/js/simple_seekbar/SimpleSeekBar.kt
@@ -1,9 +1,26 @@
 package com.js.simple_seekbar
 
+import android.content.Context
+import android.util.AttributeSet
 import android.util.Log
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 
-class SimpleSeekBar {
-    fun test() {
-        Log.d(javaClass.simpleName, "Test OK")
+class SimpleSeekBar : ConstraintLayout {
+
+    private var tvTitle: TextView
+
+    constructor(context: Context) : super(context)
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
+
+    init {
+        inflate(context, R.layout.layout_simple_seek_bar, this)
+
+        tvTitle = findViewById(R.id.tv_title)
+    }
+
+    fun setText(text: String) {
+        tvTitle.text = text
     }
 }
