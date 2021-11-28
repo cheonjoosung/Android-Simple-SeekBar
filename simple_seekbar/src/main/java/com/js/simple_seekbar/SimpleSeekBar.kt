5c30653f7b21fcf7d8c8aa798c9cddd726da2c41
cheonjoosung <cjswntjd@gmail.com>
2021-11-28 15:40:34 +0900

    SeekBar - 레이아웃, 드로어블 파일 추가, 뷰 연동

--- simple_seekbar/src/main/java/com/js/simple_seekbar/SimpleSeekBar.kt
+++ simple_seekbar/src/main/java/com/js/simple_seekbar/SimpleSeekBar.kt
@@ -2,13 +2,23 @@ package com.js.simple_seekbar
 
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
+import android.view.View
 import android.widget.TextView
 import androidx.constraintlayout.widget.ConstraintLayout
 
 class SimpleSeekBar : ConstraintLayout {
 
-    private var tvTitle: TextView
+    private var vSeekBarBase: View
+    private var vSeekBarSecond: View
+
+    private var vSeekBarDot: View
+    private var vSeekBarTriangle: View
+
+    private var tvMinText: TextView
+    private var tvMidText: TextView
+    private var tvMaxText: TextView
+
+    private var tvCurrentText: TextView
 
     constructor(context: Context) : super(context)
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs)
@@ -17,10 +27,17 @@ class SimpleSeekBar : ConstraintLayout {
     init {
         inflate(context, R.layout.layout_simple_seek_bar, this)
 
-        tvTitle = findViewById(R.id.tv_title)
+        vSeekBarBase = findViewById(R.id.v_seek_bar_base)
+        vSeekBarSecond = findViewById(R.id.v_seek_bar_second)
+
+        vSeekBarDot = findViewById(R.id.v_seek_bar_dot)
+        vSeekBarTriangle = findViewById(R.id.v_seek_bar_triangle)
+
+        tvMinText = findViewById(R.id.tv_min_text)
+        tvMidText = findViewById(R.id.tv_mid_text)
+        tvMaxText = findViewById(R.id.tv_max_text)
+
+        tvCurrentText = findViewById(R.id.tv_current_text)
     }
 
-    fun setText(text: String) {
-        tvTitle.text = text
-    }
 }
